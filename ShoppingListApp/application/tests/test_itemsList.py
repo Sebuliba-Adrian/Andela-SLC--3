773a2844b98bf737975dcc37157cac857e3cdd68
Adrian Sebuliba <adrian.sebuliba1@gmail.com>
2017-08-15 21:32:25 +0300

    Added test case for instance creation

--- ShoppingListApp/application/tests/test_itemsList.py
+++ ShoppingListApp/application/tests/test_itemsList.py
@@ -1,2 +1,15 @@
-class ItemListTest(object):
-    pass
+import unittest
+from application.models.shoppingList import ItemsList
+
+class ItemListTest(unittest.TestCase):
+
+    def setUp(self):
+        self.itemsList = ItemsList("")
+
+    def test_itemsList_instantiation(self):
+        self.assertIsInstance(self.itemsList, ItemsList, "Failed to instantiate")
+
+
+
+
+
