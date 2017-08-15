9168f5baf75b6132d25d4f1e54fd4fd1ac57e5fd
Adrian Sebuliba <adrian.sebuliba1@gmail.com>
2017-08-15 20:16:39 +0300

    Added test case for item should not be greater than 60

--- ShoppingListApp/application/tests/test_user.py
+++ ShoppingListApp/application/tests/test_user.py
@@ -1,21 +1,24 @@
 import unittest
 from application.models.user import User
 
+
 class UserTest(unittest.TestCase):
     def setUp(self):
-        self.user = User("","","")
+        self.user = User("", "", "")
 
-
+    
     def test_add_itemsList_None(self):
-         self.assertEqual(self.user.add_itemsList(None), "None input")
+        self.assertEqual(self.user.add_itemsList(None), "None input")
 
     def test_add_itemsList_blank_input(self):
         self.assertEqual(self.user.add_itemsList(" "), "Blank input")
 
     def test_add_itemsList_should_be_between10and60(self):
-         self.assertEqual(self.user.add_itemsList("shortname"),
+        self.assertEqual(self.user.add_itemsList("shortname"),
                          "ItemsList name should be greater than 10 and less than 60 characters")
 
+        self.assertNotEqual(self.user.add_itemsList("long name long name long name long name long name long name long name"),
+                         "ItemsListt name should be greater than 10 and less than 60 characters")
 
 
 
