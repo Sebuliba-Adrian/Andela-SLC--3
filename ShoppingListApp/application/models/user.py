0b3501b9b9c2452dc8ded63ac0cca6fb346a0b95
Adrian Sebuliba <adrian.sebuliba1@gmail.com>
2017-08-15 15:42:52 +0300

    Added add_itemsList method

--- ShoppingListApp/application/models/user.py
+++ ShoppingListApp/application/models/user.py
@@ -1,2 +1,10 @@
 class User(object):
-    pass
+    def __init__(self, name, username, password):
+        self.name = name
+        self.username = username
+        self.password = password
+        self.itemsList = {}
+
+
+    def add_itemsList(self,items_list_title):
+        return "None input"
