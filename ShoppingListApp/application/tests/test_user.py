6606f29aaa7e3763f399c78d3d0a7c03a179e8b4
Adrian Sebuliba <adrian.sebuliba1@gmail.com>
2017-08-15 15:43:57 +0300

    Added test for  add_items_list method with input None

--- ShoppingListApp/application/tests/test_user.py
+++ ShoppingListApp/application/tests/test_user.py
@@ -1,5 +1,18 @@
 import unittest
+from application.models.user import User
 
-class UserTest(object):
+class UserTest(unittest.TestCase):
     def setUp(self):
-        
+        self.user = User("","","")
+
+
+    def test_add_itemsList(self):
+         self.assertEqual(self.user.add_itemsList(None), "None input")
+
+
+
+
+
+
+
+
