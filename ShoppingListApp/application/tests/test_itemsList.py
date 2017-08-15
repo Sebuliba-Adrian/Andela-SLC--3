5b31dae0cda1ddf191014df775abcde71a69a670
Adrian Sebuliba <adrian.sebuliba1@gmail.com>
2017-08-16 00:59:17 +0300

    Commented out method test_update_none_input f

--- ShoppingListApp/application/tests/test_itemsList.py
+++ ShoppingListApp/application/tests/test_itemsList.py
@@ -9,6 +9,8 @@ class ItemListTest(unittest.TestCase):
     def test_itemsList_instantiation(self):
         self.assertIsInstance(self.itemsList, ItemsList, "Failed to instantiate")
 
+    # def test_update_none_input(self, description, new_description):
+    #     self.assertEqual(self.itemsList.update_bucket(" ", "  "), "Blank input")
 
 
 
