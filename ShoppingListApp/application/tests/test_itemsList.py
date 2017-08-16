75603bb47bd0402959ca685be657d2ba542f51b8
Adrian Sebuliba <adrian.sebuliba1@gmail.com>
2017-08-16 04:18:26 +0300

    Added test logic for None input

--- ShoppingListApp/application/tests/test_itemsList.py
+++ ShoppingListApp/application/tests/test_itemsList.py
@@ -1,16 +1,18 @@
 import unittest
 from application.models.shoppingList import ItemsList
 
+
 class ItemListTest(unittest.TestCase):
 
     def setUp(self):
         self.itemsList = ItemsList("")
 
     def test_itemsList_instantiation(self):
-        self.assertIsInstance(self.itemsList, ItemsList, "Failed to instantiate")
+        self.assertIsInstance(self.itemsList, ItemsList,
+                              "Failed to instantiate")
 
     def test_add_item_none(self):
-       self.assertEqual(self.itemsList.add_item(None), "None input")
+        self.assertEqual(self.itemsList.add_item(None), "None input")
 
     def test_add_item_blank(self):
         self.assertEqual(self.itemsList.add_item(" "), "Blank input")
@@ -20,17 +22,12 @@ class ItemListTest(unittest.TestCase):
 
     def test_add_item_exists(self):
         self.itemsList.add_item("mangoes")
-        self.assertEqual(self.itemsList.add_item("mangoes"), "Item already exists")
+        self.assertEqual(self.itemsList.add_item(
+            "mangoes"), "Item already exists")
 
+
+
+    def test_update_description_none(self):
+        self.assertEqual(self.itemsList.update_description(None, None), "None input")
 
     
-
-
-
-
-
-
-
-
-
-
