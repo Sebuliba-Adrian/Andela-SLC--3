f4af7a0f5247a3ba664b332e403d074e8acf16d1
Adrian Sebuliba <adrian.sebuliba1@gmail.com>
2017-08-16 09:50:03 +0300

    Added test logic for creating the instance of Item

--- ShoppingListApp/application/tests/test_item.py
+++ ShoppingListApp/application/tests/test_item.py
@@ -5,4 +5,9 @@ class ItemTest(unittest.TestCase):
     def setUp(self):
         self.item = Item("")
 
-        
+
+    def test_create_item_instance(self):
+
+        self.assertIsInstance(self.item, Item, "Failed to create instance")
+
+
