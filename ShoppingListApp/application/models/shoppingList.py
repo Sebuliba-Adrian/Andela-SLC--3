610f49f92f1973b72389828e5e11091326e0509c
Adrian Sebuliba <adrian.sebuliba1@gmail.com>
2017-08-16 03:30:10 +0300

    Added add_item method  to return 'None input'

--- ShoppingListApp/application/models/shoppingList.py
+++ ShoppingListApp/application/models/shoppingList.py
@@ -1,7 +1,21 @@
+
+from application.models.item import Item
 class ItemsList(object):
     def __init__(self, title):
         self.title = title
         self.items = {}
+
+
+    def add_item(self, description):
+        """ Adds an Item to a items """
+        if description:
+            if description.strip():
+                if not description in self.items:
+                    self.items[description] = Item(description)
+                    pass
+                pass
+            pass
+        return "None input"
 
 
 
