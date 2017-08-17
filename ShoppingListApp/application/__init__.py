52ed11786e66e6b5fa4bef441b1c8dbd6435542c
Adrian Sebuliba <adrian.sebuliba1@gmail.com>
2017-08-17 15:36:22 +0300

    Restructured the project by removing flask blueprints

--- ShoppingListApp/application/__init__.py
+++ ShoppingListApp/application/__init__.py
@@ -1,16 +1,7 @@
 from flask import Flask
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 
 app.config.from_pyfile('flask.cfg')
 
 
-from application.api.views import api_blueprint
-from application.shoppingList.views import itemslist_blueprint
-from application.user.views import user_blueprint
 
-
-app.register_blueprint(api_blueprint, url_prefix ='/api')
-app.register_blueprint(itemslist_blueprint,url_prefix='/auth')
-app.register_blueprint(user_blueprint,url_prefix='/auth')
-
-#http://localhost:5000/auth/login.
--- ShoppingListApp/application/shoppingList/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-itemslist_blueprint = Blueprint('shoppingList', __name__,template_folder='templates')
-from . import views
